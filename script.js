86b01ef6efa2e28cd63eb67d7b9cb06619a103f2
foxt7ot <myaseen.khan@hotmail.com>
2015-07-03 17:18:14 +0530

    changed something i don't know what :smile:

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
- //Do What The Fuck You Want To Public License [WTFPL]: http://sam.zoy.org/wtfpl
- //CopyrightLeft (ↄ) 2015 Yaseen <myaseen.khan@hotmail.com> 
+﻿ //CopyrightLeft (ↄ) 2015 Yaseen <myaseen.khan@hotmail.com> 
 
 function showAlert(){
 	var popupWindow=document.getElementById('modalWindow');
